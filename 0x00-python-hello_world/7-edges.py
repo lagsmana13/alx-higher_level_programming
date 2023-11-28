d507e5995e251a7c24d707979c942548632627ec
lagsmana13 <lagsmana@yahoo.com>
2023-11-27 22:52:44 -0800

    58

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
-
-word = "Holberton"
-word_first_3 = word[:3]
-
-return (1); }
-
-return (0); }
+#!/usr/bin/python3
+wrd = "Holberton"
+wrd_frst = wrd[:3]
+wrd_lst = wrd[-2:]
+mid_wrd = wrd[1:-1]
+print(f"First 3 letters: {wrd_frst}")
+print(f"Last 2 letters: {wrd_frst}")
+print(f"Middle word: {mid_wrd}")
