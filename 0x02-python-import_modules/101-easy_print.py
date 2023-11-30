9b07b25958c425859c0dbdd2e278db6182ffbdcf
lagsmana13 <lagsmana@yahoo.com>
2023-11-30 04:43:02 -0800

    654

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-
-import os
-
-def write_to_stdout(message):
-    os.write(1, message.encode("UTF-8"))
-
-if __name__ == "__main__":
-    write_to_stdout("#pythoniscool\n")
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
