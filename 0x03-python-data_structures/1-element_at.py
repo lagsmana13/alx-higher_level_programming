750e1985ac9335de8bbcdc338ee61a323b13fe55
lagsmana13 <lagsmana@yahoo.com>
2023-12-04 03:11:03 -0800

    hgd

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@
 # This function retrieves an element from a list, similar to indexing in C.
 
 def element_at(my_list, idx):
-    # Added a comment explaining the index range check
-    if idx < 0 or idx >= len(my_list):
-        return None  # Changed the return value to explicit None instead of implicit
-    return my_list[idx]  # Removed extra whitespace for consistency
+# Added a comment explaining the index range check
+if idx < 0 or idx >= len(my_list):
+return None  # Changed the return value to explicit None instead of implicit
+return my_list[idx]  # Removed extra whitespace for consistency
