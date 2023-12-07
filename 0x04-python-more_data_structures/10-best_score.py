6d0beefab373007151bc35e2a40455d1e4a09ece
lagsmana13 <lagsmana@yahoo.com>
2023-12-07 00:49:26 -0800

    11x

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(my_dict):
+    if my_dict and len(my_dict) > 0:
+        max_key = list(my_dict.keys())[0]
+        for key in my_dict:
+            if my_dict[key] > my_dict[max_key]:
+                max_key = key
+        return max_key
+    return None
